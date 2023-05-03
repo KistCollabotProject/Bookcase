a60068bc86bb22485194aad70099c58e0d24af5f
Jong <tmdwhd619@naver.com>
2023-05-03 21:05:38 +0900

    [Feat] Fix the code

--- sceinaro_make.ino
+++ sceinaro_make.ino
@@ -1,7 +1,8 @@
 #include <ros.h>
 #include <std_msgs/String.h>
 #include <DynamixelWorkbench.h>
-#include <string>
+#include<string>
+
 
 #if defined(__OPENCM904__)
 #define DEVICE_NAME "3" //Dynamixel on Serial3(USART3)  <-OpenCM 485EXP
@@ -23,10 +24,10 @@
 //#define MOTOR10 10  연결x
 
 
+
 //make publisher
 std_msgs::String moter_num //string type으로 bookcase_num topic날림
 ros::Publisher sceinaro_make("bookcase_num",  &moter_num)
-
 
 DynamixelWorkbench dxl_wb;
 ros::NodeHandle nh;
@@ -51,147 +52,17 @@ int initial_10 = 0;
 uint8_t motor[13] = {0, MOTOR1, MOTOR2, MOTOR3, MOTOR4, MOTOR5, MOTOR6, MOTOR7, MOTOR8, MOTOR9};
 
 
-
 void setup() {
 
   nh.initNode();
-  nh.advertise(); //publisher node 생성
+  nh.advertise();//publisher node 생성
   
   Serial.begin(9600);
   Serial2.begin(9600);
 }
 
-void inital_func(){
-  if (initial_1 == 0) {
-    initial_pos[1] = presentposition[1];
-    initial_1++;
-  }
-  
-  if (initial_2 == 0) {
-    initial_pos[2] = presentposition[2];
-    initial_2++;
-  }
-  
-  if (initial_3 == 0) {
-    initial_pos[3] = presentposition[3];
-    initial_3++;
-  } 
-  
-  if (initial_4 == 0) {
-    initial_pos[4] = presentposition[4];
-    initial_4++;
-  }
-
-  if (initial_5 == 0) {
-    initial_pos[5] = presentposition[5];
-    initial_5++;
-  }
-
-  if (initial_6 == 0) {
-    initial_pos[6] = presentposition[6];
-    initial_6++;
-  }
-
-  if (initial_7 == 0) {
-    initial_pos[7] = presentposition[7];
-    initial_7++;
-  }
-
-  if (initial_8 == 0) {
-    initial_pos[8] = presentposition[8];
-    initial_8++;
-  }
-
-  if (initial_9 == 0) {
-    initial_pos[9] = presentposition[9];
-    initial_9++;
-  }
-
-}
-
-
-void moter_motion(String tmp_data){
-  switch(tmp_data){
-    case "book1":
-      dxl_wb.goalPosition(motor[1], (int32_t)(initial_pos[1] + 7900));
-      motor_open[0] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[1], (int32_t)(initial_pos[1] + 100));
-      motor_open[0] = 0;
-      break;
-    
-    case "book2":
-      dxl_wb.goalPosition(motor[2], (int32_t)(initial_pos[2] + 7900));
-      motor_open[1] = 1;
-      delay(30000);
-      dxl_wb.goalPosition(motor[2], (int32_t)(initial_pos[2] + 100));
-      motor_open[1] = 0;
-      break;
-
-    case "book3":
-      dxl_wb.goalPosition(motor[3], (int32_t)(initial_pos[3] + 7900));
-      motor_open[2] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[3], (int32_t)(initial_pos[3] + 100));
-      motor_open[2] = 0;
-      break;
-
-    case "book4":
-      dxl_wb.goalPosition(motor[4], (int32_t)(initial_pos[4] + 7900));
-      motor_open[3] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[4], (int32_t)(initial_pos[4] + 100));
-      motor_open[3] = 0;
-      break;
-
-    case "book5":
-      dxl_wb.goalPosition(motor[5], (int32_t)(initial_pos[5] + 7900));
-      motor_open[4] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[5], (int32_t)(initial_pos[5] + 100));
-      motor_open[4] = 0;
-      break;
-
-    case "book6":
-      dxl_wb.goalPosition(motor[6], (int32_t)(initial_pos[6] +7900));
-      motor_open[5] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[6], (int32_t)(initial_pos[6] + 100));
-      motor_open[5] = 0;
-      break;
-
-    case "book7":
-      dxl_wb.goalPosition(motor[7], (int32_t)(initial_pos[7] +7900));
-      motor_open[6] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[7], (int32_t)(initial_pos[7] + 100));
-      motor_open[6] = 0;
-      break;
-
-    case "book8":
-      dxl_wb.goalPosition(motor[8], (int32_t)(initial_pos[8] +7900));
-      motor_open[7] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[8], (int32_t)(initial_pos[8] + 100));
-      motor_open[7] = 0;
-      break;
-
-    case "book9":
-      dxl_wb.goalPosition(motor[9], (int32_t)(initial_pos[9] +7900));
-      motor_open[8] = 1;
-      delay(6000);
-      dxl_wb.goalPosition(motor[9], (int32_t)(initial_pos[9] + 100));
-      motor_open[8] = 0;
-      break;
-
-    default:
-      break;
-  }
-  sceinaro_make.publish(&tmp_data)
-}
-
 void loop() {
-  std_msgs::String data;
+
   const char *log;
   dxl_wb.init(DEVICE_NAME, BAUDRATE, &log);
 
@@ -239,15 +110,132 @@ void loop() {
   dxl_wb.getPresentPositionData(motor[9], &presentposition[9], &log);
   //dxl_wb.getPresentPositionData(motor[10], &presentposition[10], &log);
 
-  inital_func()
+  if (initial_1 == 0) {
+    initial_pos[1] = presentposition[1];
+    initial_1++;
+  }
+  
+  if (initial_2 == 0) {
+    initial_pos[2] = presentposition[2];
+    initial_2++;
+  }
+  
+  if (initial_3 == 0) {
+    initial_pos[3] = presentposition[3];
+    initial_3++;
+  } 
+  
+  if (initial_4 == 0) {
+    initial_pos[4] = presentposition[4];
+    initial_4++;
+  }
 
+  if (initial_5 == 0) {
+    initial_pos[5] = presentposition[5];
+    initial_5++;
+  }
+
+  if (initial_6 == 0) {
+    initial_pos[6] = presentposition[6];
+    initial_6++;
+  }
+
+  if (initial_7 == 0) {
+    initial_pos[7] = presentposition[7];
+    initial_7++;
+  }
+
+  if (initial_8 == 0) {
+    initial_pos[8] = presentposition[8];
+    initial_8++;
+  }
+
+  if (initial_9 == 0) {
+    initial_pos[9] = presentposition[9];
+    initial_9++;
+  }
+
+  
   while (Serial2.available()) {
-    data = Serial2.readStringUntil(' ');
-    moter_motion(data)
 
-    //nh.spinOnce();
+    String data = Serial2.readStringUntil(' ');
+    if (data == "book1") {
+      dxl_wb.goalPosition(motor[1], (int32_t)(initial_pos[1] + 7900));
+      motor_open[0] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[1], (int32_t)(initial_pos[1] + 100));
+      motor_open[0] = 0;
+    }
+    
+    if (data == "book2") {
+      dxl_wb.goalPosition(motor[2], (int32_t)(initial_pos[2] + 7900));
+      motor_open[1] = 1;
+      delay(30000);
+      dxl_wb.goalPosition(motor[2], (int32_t)(initial_pos[2] + 100));
+      motor_open[1] = 0;
+    }
+
+    if (data == "book3") {
+      dxl_wb.goalPosition(motor[3], (int32_t)(initial_pos[3] + 7900));
+      motor_open[2] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[3], (int32_t)(initial_pos[3] + 100));
+      motor_open[2] = 0;
+    }
+
+    if (data == "book4") {
+      dxl_wb.goalPosition(motor[4], (int32_t)(initial_pos[4] + 7900));
+      motor_open[3] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[4], (int32_t)(initial_pos[4] + 100));
+      motor_open[3] = 0;
+    }
+
+    if (data == "book5") {
+      dxl_wb.goalPosition(motor[5], (int32_t)(initial_pos[5] + 7900));
+      motor_open[4] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[5], (int32_t)(initial_pos[5] + 100));
+      motor_open[4] = 0;
+    }
+
+    if (data == "book6") {
+      dxl_wb.goalPosition(motor[6], (int32_t)(initial_pos[6] +7900));
+      motor_open[5] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[6], (int32_t)(initial_pos[6] + 100));
+      motor_open[5] = 0;
+    }
+
+    if (data == "book7") {
+      dxl_wb.goalPosition(motor[7], (int32_t)(initial_pos[7] +7900));
+      motor_open[6] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[7], (int32_t)(initial_pos[7] + 100));
+      motor_open[6] = 0;
+    }
+
+    if (data == "book8") {
+      dxl_wb.goalPosition(motor[8], (int32_t)(initial_pos[8] +7900));
+      motor_open[7] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[8], (int32_t)(initial_pos[8] + 100));
+      motor_open[7] = 0;
+    }
+
+    if (data == "book9") {
+      dxl_wb.goalPosition(motor[9], (int32_t)(initial_pos[9] +7900));
+      motor_open[8] = 1;
+      delay(6000);
+      dxl_wb.goalPosition(motor[9], (int32_t)(initial_pos[9] + 100));
+      motor_open[8] = 0;
+    }
+
+    sceinaro_make.publish(&data)
+     nh.spinOnce();
 
 
   }
 
 }
+
